cb7e44e090618df6830bb8598a838fabc1ca695c
safissl <safissl@gmail.com>
2018-11-02 00:06:02 +0100

    HW3 - bash scripts

--- README.md
+++
@@ -1,2 +0,0 @@
-# MNXB01-2018
-GIT repository for the Programming Course MNXB01-2018 at Lund University
--- floridop/Tutorial3/bash/getcpuinfo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# put the output of cat in the variable CPUINFO
-CPUINFO=$(cat /proc/cpuinfo | head -10)
-
-# write the content of CPUINFO to screen
-echo "$CPUINFO"
